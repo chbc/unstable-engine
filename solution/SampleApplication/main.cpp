ae07e78a40071694d584ea1e64184ab6fa61d80a
Henrique <henrique.dsc@gmail.com>
2016-02-14 21:59:58 -0200

    Removed application files from SRE. Added new SampleApplication project.

--- solution/SampleApplication/main.cpp
+++ solution/SampleApplication/main.cpp
@@ -3,19 +3,22 @@
 
 int main(int argc, char *argv[])
 {
+	int result = 0;
 	try
 	{
 		SampleApplication().run();
-		return 0;
 	}
 	catch (const std::string &message)
 	{
 		std::cout << "Error: " << message << std::endl;
-		return 1;
+		result = 1;
 	}
 	catch (std::exception &ex)
 	{
 		std::cout << "Error: " << ex.what() << std::endl;
-		return 1;
+		result = 2;
 	}
+	
+	system("pause");
+	return result;
 }
