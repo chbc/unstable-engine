d3a63385f2b413e354339f7e95f27b2c8033df0f
Henrique <henrique.cunha@dioramaxd.com>
2025-06-19 14:26:40 -0300

    Fix FileIcons folder name (in progress).

--- solution/RenderEngine/include/core_defines.h
+++ solution/RenderEngine/include/core_defines.h
@@ -3,7 +3,7 @@
 #ifdef __ANDROID__
 	#define SRE_API
 #elif SRE_DLL_EXPORT
-	#define SRE_API __declspec( dllexport )
+	#define SRE_API __declspec(dllexport)
 #else
 	#define SRE_API __declspec(dllimport)
 #endif
