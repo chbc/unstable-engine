9ce019d6999b4509b9ba0f698cdcfcdf9993ae3b
Henrique <henrique.dsc@gmail.com>
2026-07-19 20:10:16 -0300

    updated CMakeLists.txt and build script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 BUILD_DIR="build"
+BIN_DIR="bin"
 
 echo "--- Limpando ambiente ---"
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
+
+rm -rf $BIN_DIR
+mkdir -p $BIN_DIR
 
 echo "--- Iniciando configuração com CMake ---"
 cd $BUILD_DIR
